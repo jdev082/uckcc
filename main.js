1ea94db4db381da9eec3461708067101d9c7ce80
Jayden W <willsjayden2@gmail.com>
2023-02-01 15:08:30 -0600

    feat: linter, typescript, and fixes

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 try {
-	require('electron-reloader')(module);
+    require('electron-reloader')(module);
 } catch {}
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // preload: path.join(__dirname, 'preload.js')
-    }
-  })
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            // preload: path.join(__dirname, 'preload.js')
+        }
+    });
 
-  win.loadFile('./src/index.html')//,
-  win.setMenu(null)
+    win.loadFile('./src/index.html');//,
+    win.setMenu(null);
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
--- src/socket.ts
+++ src/socket.ts
@@ -1,82 +1,94 @@
 // configration (touch this)
-var server = 'wss://server.mattkc.com:2002';
-var chat = new WebSocket(''.concat(server));
-var token = ''; // DO NOT SHARE THIS!
-var loginMessage = 'Logged in using UCKCC!';
+const server = 'wss://server.mattkc.com:2002';
+const chat = new WebSocket(`${server}`);
+const token = ''; // DO NOT SHARE THIS!
+const loginMessage = 'Logged in using UCKCC!';
+
 // code (touch this if you know what you are doing)
-var input = document.querySelector('#msgbox');
-var srvbox = document.querySelector('#srvbox');
-var jmsgb = document.querySelector('#jmsgb');
-var body = document.getElementsByTagName('body');
+const input = document.querySelector('#msgbox');
+const srvbox = document.querySelector('#srvbox');
+const jmsgb = document.querySelector('#jmsgb');
+const body = document.getElementsByTagName('body');
 srvbox.value = server;
+
 function readySend() {
     chat.send(JSON.stringify({ type: 'status', data: {}, auth: 'google', token: token }));
     chat.send(JSON.stringify({ type: 'getuserconf', data: {}, auth: 'google', token: token }));
 }
+
 function sendMessage(msg) {
     chat.send(JSON.stringify({ type: 'message', data: { text: msg }, reply: 0, auth: 'google', token: token }));
 }
+
 function setServer(addr) {
-    var server = 'wss://' + addr;
-    var chat = new WebSocket(''.concat(server));
+    const server = 'wss://' + addr;
+    const chat = new WebSocket(`${server}`);
 }
+
 function renderMessage(author, message) {
     render = document.createElement('p');
     render.innerText = author + ': ' + message;
     render.classList.add = 'message';
     document.body.appendChild(render);
 }
+
 function randBgColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
+    const x = Math.floor(Math.random() * 256);
+    const y = Math.floor(Math.random() * 256);
+    const z = Math.floor(Math.random() * 256);
     return 'rgb(' + x + ',' + y + ',' + z + ')';
 }
+
 chat.onopen = function () {
     readySend();
-    console.log('SUCCESS: Connected to server '.concat(server));
+    console.log(`SUCCESS: Connected to server ${server}`);
     if (localStorage.getItem('sendJoinMessage') === 'true') {
         //sendMessage(`${loginMessage}`)
     }
 };
+
 chat.onmessage = function (e) {
-    var packet = JSON.parse(e.data);
+    const packet = JSON.parse(e.data);
     console.log(packet);
-    console.log('DEBUG: [message] Data received from socket: '.concat(e.data));
+    console.log(`DEBUG: [message] Data received from socket: ${e.data}`);
     if (packet.type == 'chat') {
         renderMessage(packet.data.author, packet.data.message);
     }
 };
+
 setInterval(readySend, 10000);
+
 input.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        sendMessage(''.concat(input.value));
+        sendMessage(`${input.value}`);
         input.value = '';
     }
 });
+
 srvbox.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-        setServer(''.concat(srvbox.value));
+        setServer(`${srvbox.value}`);
     }
 });
+
 jmsgb.addEventListener('change', function () {
     if (this.checked) {
         localStorage.setItem('sendJoinMessage', true);
-    }
-    else {
+    } else {
         localStorage.setItem('sendJoinMessage', false);
     }
 });
+
 if (localStorage.getItem('sendJoinMessage') === undefined) {
     localStorage.setItem('sendJoinMessage', false);
 }
+
 if (localStorage.getItem('sendJoinMessage' === 'true')) {
     jmsgb.checked = 'true';
-}
-else {
+} else {
     jmsgb.checked == 'false';
 }
-document.body.addEventListener('click', function () {
+
+document.body.addEventListener('click', function() {
     document.body.style.background = randBgColor();
 });
-//# sourceMappingURL=socket.js.map
